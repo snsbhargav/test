c4b0856e0d0bca3bb0a7e2e1c5d6faf84f7528a0
Bhargav Siddineni <snsbhargav@gmail.com>
2026-05-18 22:39:15 -0400

    initial commit

---
+++ src/main/java/com/event/contact/app/controller/demo.java
@@ -0,0 +1,13 @@
+package com.event.contact.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class demo {
+
+	@GetMapping("/")
+	public String demo1() {
+		return "Backend Working";
+	}
+}
